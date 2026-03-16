368d1345311f06fa7e2bd353ae76dd5e0612f6b1
CL5534 <cof5534@gmail.com>
2026-03-16 14:01:17 +0900

    테스트1

--- front/src/components/home/HomeHeroSection.jsx
+++ front/src/components/home/HomeHeroSection.jsx
@@ -11,7 +11,7 @@ function HomeHeroSection() {
           "linear-gradient(180deg, rgba(20, 16, 14, 0.12) 0%, rgba(20, 16, 14, 0.58) 100%), url('https://images.unsplash.com/photo-1516734212186-a967f81ad0d7?auto=format&fit=crop&w=1600&q=80')",
       }}
     >
-           <h1>홈페이지</h1>
+           <h1>홈페이지ㄴㅇㄴㅇ</h1>
       <div className="heroBannerContent">
         <p className="sectionEyebrow heroBannerEyebrow">PetHotel Main</p>
         <h1>편안한 하루를 맡기고, 안심되는 케어를 바로 확인해보세요.</h1>
